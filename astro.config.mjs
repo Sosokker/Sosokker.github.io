24a213dbeb779e425eb11df7a108e7efa913d3e1
sosokker <sirin_29@hotmail.co.th>
2023-09-13 20:22:56 +0700

    astro try 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,5 @@
 import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-import { remarkReadingTime } from './src/utils/readTime.ts'
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://sosokker.github.io', // Write here your website url
-	experimental: {
-		assets: true
-	},
-	markdown: {
-		remarkPlugins: [remarkReadingTime],
-		drafts: true,
-		shikiConfig: {
-			theme: 'github-dark-dimmed',
-			wrap: true
-		}
-	},
-	integrations: [
-		mdx({
-			syntaxHighlight: 'shiki',
-			shikiConfig: {
-				theme: 'github-dark-dimmed',
-				wrap: true
-			},
-			drafts: true
-		}),
-		sitemap(),
-		tailwind()
-	]
+  site: 'https://sosokker.github.io',
 })
