477399bd2ab250846be6954082a59178b922856c
sosokker <sirin_29@hotmail.co.th>
2023-09-20 19:01:16 +0700

    Quartz sync: Sep 20, 2023, 7:01 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -20,9 +20,9 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
+          light: "#f5f5f5",
           lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
+          gray: "#646464",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
           secondary: "#284b63",
@@ -30,7 +30,7 @@ const config: QuartzConfig = {
           highlight: "rgba(143, 159, 169, 0.15)",
         },
         darkMode: {
-          light: "#161618",
+          light: "#181a1b",
           lightgray: "#393639",
           gray: "#646464",
           darkgray: "#d4d4d4",
